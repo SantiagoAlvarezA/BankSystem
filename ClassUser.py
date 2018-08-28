881471fdccd19361f7cf315bc16731e8bcdb3951
Santiago Alvarez <aalvarez3570@gmail.com>
2018-08-28 00:09:53 -0500

    reporte de cuotas pagadas

--- ClassUser.py
+++ ClassUser.py
@@ -3,57 +3,151 @@ dicLending = {}
 mainDic = {'Users': dicUser, 'Lendings': dicLending}
 
 
+def is_number(msg):
+    isNum = False
+    num = 0
+    while not isNum:
+        try:
+            num = int(input(msg))
+            isNum = True
+        except ValueError:
+            print('Por favor digite un valor numerico')
+    return num
+
+
 class Lending:
-    id = 0
     idUser = 0
     valLending = 0
     fee = 0
+    userExist = False
 
-    def createLending(self):
-        self.idUser = int(input('digite el numero de documento de la persona solicitante: '))
-        self.valLending = int(input('Que valor desea solicitar para este prestamo: '))
-        self.fee = int(input('en cuantas cuotas desea cancelar el prestamo? '))
-        val = self.valLending / self.fee
+    def create_lending(self):
 
-        dicFee = {}
+        if not dicUser:
+            print('No existen usuarios registrados en el sistema')
+        else:
+            while not self.userExist:
+                self.idUser = is_number('Digite el número de documento de la persona solicitante: ')
+
+                for us in dicUser:
+
+                    if us == self.idUser:
+                        self.userExist = True
+
+                if self.userExist:
+
+                    # validar cuando el usuario aun no termine de pagar las cuotas
+                    self.valLending = is_number('Que valor desea solicitar para este prestamo: ')
+                    self.fee = is_number('En cuantas cuotas desea cancelar el prestamo? ')
+                    val = self.valLending / self.fee
+
+                    dicFee = {}
+                    for fee in range(0, self.fee):
+                        dicFee[fee + 1] = {'value': val, 'status': False}
+
+                    dicLending[self.idUser] = dicFee
+                else:
+                    print('La persona solicitante no esta registrada')
+        self.userExist = False
 
 
-        for fee in range(0, self.fee):
-            dicFee[fee] = {'value': val, 'status': False}
+class Pay:
+    idUser = 0
+    selection = 0
+    arrayFee = []
+    pay = False
 
-        dicLending[self.idUser] = {'valLending': self.valLending, 'fee': dicFee}
+    def pay_fee(self):
+
+        if not dicUser:
+            print('No hay usuarios registrados')
+        else:
+
+            if not dicLending:
+                print('Hasta el momento no se a realizado ningun prestamo')
+            else:
+
+                self.idUser = is_number('Digite el documento de la persona que realizara el pago: ')
+
+                for lending in dicLending:
+
+                    if lending == self.idUser:
+
+                        for fee in dicLending[lending]:
+
+                            if not dicLending[lending][fee]['status']:
+                                self.pay = True
+                                self.arrayFee.append(fee)
+                                print('Cuota sin pagar numero {} por un valor de {} '
+                                      .format(fee, dicLending[lending][fee]['value']))
+
+        if self.pay:
+
+            paid = False
+
+            while not paid:
+
+                numFee = is_number('Digite el numero de la cuota que desea pagar: ')
+
+                for nF in self.arrayFee:
+                    if nF == numFee:
+                        dicLending[self.idUser][numFee]['status'] = True
+                        paid = True
+        self.pay = False
 
 
-# class Pay:
-#
-#
-#     def payFee:
+class Report:
+    idUser = 0
+
+    def paid_fees(self):
+        if not dicUser:
+            print('No existen usuarios registrados en el sistema')
+        else:
+            if not dicLending:
+                print('No hay prestamos registrados')
+            else:
+                self.idUser = is_number('Digite el numero de documento: ')
+                if dicUser[self.idUser]:
+                    if dicLending[self.idUser]:
+                        totalPaid = 0
+                        value = 0
+                        pay = 0
+                        for fee in dicLending[self.idUser]:
+                            if dicLending[self.idUser][fee]['status']:
+                                totalPaid = totalPaid + 1
+                                pay = dicLending[self.idUser][fee]['value']
+                                value = value + dicLending[self.idUser][fee]['value']
+                        print("""
+                        Usted a pagado {} cuotas por un valor de {} 
+                        que suma un total de {}
+                        """.format(totalPaid, pay, value))
+
+                    else:
+                        print('Este usuario no ha solicitado prestamos')
+
+                else:
+                    print('Este usuario no se encuentra registrado')
 
 
-
-
-
-class User(Lending):
+class User(Lending, Pay, Report):
     name = ''
     lastName = ''
     document = 0
     address = ''
 
-    def createUser(self):
+    def create_user(self):
         self.name = input('digite su nombre: ')
         self.lastName = input('digite sus apellidos: ')
-        self.document = int(input('digite el numero de documento: '))
+        self.document = is_number('digite el numero de documento: ')
         self.address = input('digite su direccion de recidensia: ')
         dicUser[self.document] = {'name': self.name, 'lastName': self.lastName, 'address': self.address}
 
 
 user = User()
-user.createUser()
-user.createLending()
-print(dicUser)
-print()
-print(dicLending)
-
-print()
-
+user.create_user()
+user.create_lending()
+user.pay_fee()
+user.pay_fee()
+user.pay_fee()
+user.paid_fees()
 print(mainDic)
